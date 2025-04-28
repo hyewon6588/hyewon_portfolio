77e14329145b182517a47ef48244beae7a518232
hyewon6588 <hhan29@my.centennialcollege.ca>
2025-04-28 09:27:24 -0400

    added emailjs service

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -68,7 +68,7 @@ export default function ContactPage() {
             </h1>
           </div>
           <p className="text-gray-600 text-sm mb-6">
-            Send a message to the developer from your region.
+            Let's get in touch. Drop me a line!
           </p>
 
           <form onSubmit={handleSubmit} className="flex flex-col gap-4">
